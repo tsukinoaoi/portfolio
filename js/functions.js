d57940d47406da898e9fa8fd9ddbb424cbb5caa7
unknown <pinedacasco23@gmail.com>
2022-11-15 18:08:47 -0600

    loading and background cool

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+
+function sect1background() {
+   console.log("presionado");
+   document.body.style.backgroundColor = "#1e3266";
+  }
+
+  function sect2background() {
+    console.log("presionado");
+    document.body.style.backgroundColor = "#d774a0";
+   }
+ 
+   //load page 
+var myVar;
+
+function waitpage() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  window.scroll(0, 0); 
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display= "block";
+}
